7b7c6ef3336cf519194501f49a5944a8e5a9d6c4
Qharny <cannycc1020@gmail.com>
2024-01-31 08:58:50 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	new file:   src/math/roundingDouble.java
    
    Changes not staged for commit:
    	modified:   src/Main.java
    	modified:   src/MyVariable.java
    	modified:   src/math/TrigonometricFunctions.java
    	modified:   src/note.txt

---
+++ src/math/roundingDouble.java
@@ -0,0 +1,9 @@
+package math;
+
+public class roundingDouble {
+    public static void main(String[] args) {
+        double x = 76.836543332;
+        double x1 = Math.round(x*1000)/1000.0;
+        System.out.println(x1);
+    }
+}
