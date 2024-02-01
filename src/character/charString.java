5f312802dcb53bf523ce9074ddc7c950e49ea81b
Qharny <cannycc1020@gmail.com>
2024-02-01 07:59:10 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 3 commits.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	new file:   src/character/charString.java
    	modified:   src/note.txt

---
+++ src/character/charString.java
@@ -0,0 +1,9 @@
+package character;
+
+public class charString {
+    public static void main(String[] args) {
+        // Esccape sequeces in Java.
+        System.out.println("the tab key(backslack and t): \t✓");
+        System.out.println("the new line(backslack and n): \n the rest on the next line✓");
+    }
+}
