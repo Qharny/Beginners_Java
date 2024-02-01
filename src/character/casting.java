153631525b94ec512f621cc9d6c5f713d8a8646e
Qharny <cannycc1020@gmail.com>
2024-02-01 08:24:10 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 4 commits.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	new file:   src/character/casting.java
    
    Changes not staged for commit:
    	modified:   src/character/casting.java

---
+++ src/character/casting.java
@@ -0,0 +1,9 @@
+package character;
+
+public class casting {
+    public static void main(String[] args) {
+        System.out.println("27321 reps char: " +(char)27321);
+        System.out.println("20002 reps char: " +(char)20002);
+        System.out.println("1067 reps char: " +(char)1067);
+    }
+}
