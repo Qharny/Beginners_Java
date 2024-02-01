a3fc5a92acb5c6807868fc6f3815b18c6df8cd03
Qharny <cannycc1020@gmail.com>
2024-02-01 08:24:20 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 5 commits.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   src/character/casting.java

--- src/character/casting.java
+++ src/character/casting.java
@@ -5,5 +5,21 @@ public class casting {
         System.out.println("27321 reps char: " +(char)27321);
         System.out.println("20002 reps char: " +(char)20002);
         System.out.println("1067 reps char: " +(char)1067);
+        System.out.println("263 reps char: " +(char)263);
+        System.out.println("638 reps char: " +(char)638);
+
+        // NUMBERS CHARACTER ARE STORED
+
+        System.out.println("\' \' stores as num: " +(int)' ');
+        System.out.println("\' \' stores as num: " +(int)'?');
+        System.out.println("\' \' stores as num: " +(int)'A');
+        System.out.println("\' \' stores as num: " +(int)'B');
+        System.out.println("\' \' stores as num: " +(int)'a');
+        System.out.println("\' \' stores as num: " +(int)'b');
+
+        char letter = 'A';
+        char num = '2';
+        System.out.println("Is "+ letter  + " a number? "+ Character.isDigit(letter));
+        System.out.println("Is "+ num  + " a letter? "+ Character.isLetter(num));
     }
 }
