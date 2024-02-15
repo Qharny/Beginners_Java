2daa68a84624fe16916ae99d97e5fcb606bda93a
Qharny <cannycc1020@gmail.com>
2024-02-15 09:32:03 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 8 commits.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	new file:   outputFile.txt
    	modified:   src/File_handling/WriteFile.java

--- src/File_handling/WriteFile.java
+++ src/File_handling/WriteFile.java
@@ -1,2 +1,18 @@
-package File_handling;public class WriteFile {
+package File_handling;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class WriteFile {
+    public static void main(String[] args) throws FileNotFoundException {
+        PrintWriter outputFile = new PrintWriter("outputFile.txt");
+
+        // create a file and fill it with text
+        outputFile.println("This is an updated file");
+        outputFile.print("I love to code ");
+        outputFile.print("in dart.");
+
+        // close the printWriter
+        outputFile.close();
+    }
 }
