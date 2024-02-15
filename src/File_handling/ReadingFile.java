d3144e07f9b99249f9845d85436de41e8116634d
Qharny <cannycc1020@gmail.com>
2024-02-15 07:35:42 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	new file:   .idea/inspectionProfiles/Project_Default.xml
    	new file:   qodana.yaml
    	modified:   src/File_handling/ReadingFile.java

--- src/File_handling/ReadingFile.java
+++ src/File_handling/ReadingFile.java
@@ -1,2 +1,14 @@
-package File_handling;public class ReadingFile {
+package File_handling;
+
+import java.io.File;
+
+public class ReadingFile {
+    public static void main(String[] args) {
+        // get file from project folder
+        File inputFile = new File("text.txt");
+
+        // get file from desktop
+        File inputDesktopFile = new File("\"C:\\Users\\canny\\OneDrive\\Desktop\\text.txt\"");
+
+    }
 }
