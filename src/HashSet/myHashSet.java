46500fc475aa4a94873f470ae710883826381af3
Qharny <cannycc1020@gmail.com>
2024-02-28 08:00:07 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   src/HashSet/myHashSet.java

--- src/HashSet/myHashSet.java
+++ src/HashSet/myHashSet.java
@@ -1,2 +1,28 @@
-package HashSet;public class myHashSet {
+package HashSet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class myHashSet {
+    public static void main(String[] args) {
+        // Creating of hashSet
+        Set<Integer> stdAges = new HashSet<Integer>();
+        HashSet<String> stdNames = new HashSet<String>();
+        HashSet<Double> stdMarks = new HashSet<Double>();
+
+        // ADDING CONTENT TO HASHSET
+        stdAges.add(26);
+        stdAges.add(27);
+        stdMarks.add(89.7);
+        stdNames.add("Jim");
+        stdNames.add("Sam");
+
+        System.out.println(stdAges.size());
+        System.out.println(stdNames.size());
+        System.out.println(stdMarks.size());
+        System.out.println(stdAges);
+        System.out.println(stdNames);
+        System.out.println(stdMarks);
+        System.out.println(stdAges.contains(26));
+    }
 }
