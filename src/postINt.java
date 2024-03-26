e202acd005ca5473ba3cbaa0bd4088592841f529
Qharny <cannycc1020@gmail.com>
2024-03-26 22:01:54 +0000

    created positive integers

---
+++ src/postINt.java
@@ -0,0 +1,8 @@
+public class postINt {
+    public static void main(String[] args) {
+        String [] postiInt = new String[10];
+        postiInt[0] = "1";
+        postiInt[1] = "2";
+        postiInt[2] = "3";
+    }
+}
