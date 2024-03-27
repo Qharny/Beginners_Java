27d5b963712956c19756bc22f974d631f83cbbeb
Qharny <cannycc1020@gmail.com>
2024-03-27 07:50:48 +0000

    modified and created

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,66 @@
-// this one I used Ai.
-public class  Main {
+//// this one I used Ai.
+//public class  Main {
+//    public static void main(String[] args) {
+//        // (a) Maximum value
+//        double[] set = {-2.1, -1.5, -0.6};
+//        double max = findMax(set);
+//        System.out.println("Maximum value for the set (-2.1, -1.5, -0.6): " + max);
+//
+//        // (b) Minimum value
+//        double min = findMin(set);
+//        System.out.println("Minimum value for the set (-2.1, -1.5, -0.6): " + min);
+//
+//        // (c) Absolute value
+//        double num1 = -1.51;
+//        double num2 = 1.51;
+//        double abs1 = Math.abs(num1);
+//        double abs2 = Math.abs(num2);
+//        System.out.println("Absolute value for -1.51: " + abs1);
+//        System.out.println("Absolute value for 1.51: " + abs2);
+//    }
+//
+//    // Method to find maximum value in an array
+//    public static double findMax(double[] arr) {
+//        double max = arr[0];
+//        for (int i = 1; i < arr.length; i++) {
+//            if (arr[i] > max) {
+//                max = arr[i];
+//            }
+//        }
+//        return max;
+//    }
+//
+//    // Method to find minimum value in an array
+//    public static double findMin(double[] arr) {
+//        double min = arr[0];
+//        for (int i = 1; i < arr.length; i++) {
+//            if (arr[i] < min) {
+//                min = arr[i];
+//            }
+//        }
+//        return min;
+//    }
+//}
+
+public class Main {
     public static void main(String[] args) {
-        // (a) Maximum value
-        double[] set = {-2.1, -1.5, -0.6};
-        double max = findMax(set);
-        System.out.println("Maximum value for the set (-2.1, -1.5, -0.6): " + max);
+        double[][] twoDArray = new double[5][2];
 
-        // (b) Minimum value
-        double min = findMin(set);
-        System.out.println("Minimum value for the set (-2.1, -1.5, -0.6): " + min);
+        for (int row = 0; row < twoDArray.length; row++) {
+            for (int col = 0; col < twoDArray[0].length; col++) {
 
-        // (c) Absolute value
-        double num1 = -1.51;
-        double num2 = 1.51;
-        double abs1 = Math.abs(num1);
-        double abs2 = Math.abs(num2);
-        System.out.println("Absolute value for -1.51: " + abs1);
-        System.out.println("Absolute value for 1.51: " + abs2);
-    }
-
-    // Method to find maximum value in an array
-    public static double findMax(double[] arr) {
-        double max = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
+                twoDArray[row][col] = (row * 10) + col;
             }
         }
-        return max;
-    }
 
-    // Method to find minimum value in an array
-    public static double findMin(double[] arr) {
-        double min = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] < min) {
-                min = arr[i];
+        // Print out the content of the array
+        System.out.println("Content of the two-dimensional array:");
+        for (int row = 0; row < twoDArray.length; row++) {
+            for (int col = 0; col < twoDArray[row].length; col++) {
+                // Print each element separated by a comma
+                System.out.print(twoDArray[row][col] + ",");
             }
+            System.out.println();
         }
-        return min;
     }
 }
--- src/postINt.java
+++ src/postINt.java
@@ -1,8 +1,14 @@
 public class postINt {
     public static void main(String[] args) {
-        String [] postiInt = new String[10];
-        postiInt[0] = "1";
-        postiInt[1] = "2";
-        postiInt[2] = "3";
+        int [] postiInt = new int [10];
+        for(int i = 0; i < postiInt.length; i++){
+            postiInt[i] = i;
+        }
+        System.out.println(postiInt[0]);
+        int sum = 0;
+        for(int x = 0; x < postiInt.length; x++){
+            sum = sum + postiInt[x];
+        }
+        System.out.println(sum);
     }
 }
